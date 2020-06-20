2d0bf1a674cdb05d76503680fd192cfd35398dc0
Doncot <8072620+doncot@users.noreply.github.com>
2020-06-20 13:41:55 +0900

    開発者ツールが有効か否かでインデックスが変わる

--- content.js
+++ content.js
@@ -19,17 +19,22 @@ chrome.runtime.onMessage.addListener(
         for(let i = 0; i < elements.length; i++) {
             switch(i)
             {
-            case 10:
+            // case 10:
+            case 23: // 開発者ツールが有効か否かでインデックスが変わる
                 user = elements[i].innerHTML;
                 break;
-            case 11:
+            // case 11:
+            case 24:
                 id = elements[i].innerHTML;
                 break;
-            case 12:
+            // case 12:
+            case 25:
                 body = elements[i].innerHTML;
                 break;
-            case 13: // 本文がない場合はここが有効に
-            case 14:
+            // case 13: // 本文がない場合はここが有効に
+            // case 14:
+            case 26:
+            case 27:
                 // 既に日付が入った場合は無視する
                 if(date == "") {
                     var results = /((\d{4})年(\d{1,2})月(\d{1,2})日)/.exec(elements[i].innerHTML);
