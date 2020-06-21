d8cf2bd7fc5f4a65edbad456888cae9661e88013
Doncot <8072620+doncot@users.noreply.github.com>
2020-06-21 23:35:41 +0900

    複数枚のDL

--- background.js
+++ background.js
@@ -1,11 +1,38 @@
 function onPageClick(info, tab) {
+    chrome.tabs.sendMessage(tab.id, {messageType: "multiple-image"}, function(response) {
+        // メッセージ返し
+        if(response["respondType"] == "multiple-image-request") {
+            console.debug("respond tweet: " + JSON.stringify(response.tweet));
+
+            var user = response.tweet["user"];
+            var id = response.tweet["id"];
+            var body = "";
+            if(response.tweet.hasOwnProperty("body")) {
+                body = response.tweet["body"];
+            }
+            var date = response.tweet["date"];
+
+            // 複数枚
+            if(response.tweet.hasOwnProperty("images")) {
+                var index = 0;
+                for(const imageSrc of response.tweet["images"]) {
+                    var extMatches = imageSrc.match(/\?format=([^&]+)/);
+                    var ext = extMatches[1];
+
+                    var title = "[" + user + "(" + id + ")] " + body + " (" + date + "_" + (index + 1) + ")." + ext;
+
+                    var downloadUrl = imageSrc.replace(/&name=.+/, "&name=orig");
+                    console.debug(title + ": " + downloadUrl);
+                    chrome.downloads.download({"url": downloadUrl, "method": "GET", "conflictAction": "prompt", "filename" : title});
+
+                    index++;
+                }
+            }
+        }
+    });
 }
 
 function onImageClick(info, tab) {
-    // console.log("item " + info.menuItemId + " was clicked");
-    // console.log("info: " + JSON.stringify(info));
-    // console.log("tab: " + JSON.stringify(tab));
-
     chrome.tabs.sendMessage(tab.id, {messageType: "single-image"}, function(response) {
         // メッセージ返し
         if(response["respondType"] == "single-image-request") {
@@ -31,5 +58,5 @@ function onImageClick(info, tab) {
 }
 
 // 処理開始
-// chrome.contextMenus.create({"title": "画像を保存（ページ経由）", "contexts":["page"], "onclick": onPageClick});
+chrome.contextMenus.create({"title": "複数画像を保存", "contexts":["page", "image"], "onclick": onPageClick});
 chrome.contextMenus.create({"title": "画像を保存（画像経由）", "contexts":["image"], "onclick": onImageClick});
--- content.js
+++ content.js
@@ -6,63 +6,122 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.debug("on content script");
 
-        // console.log("document.all[0].outerHTML = " + document.all[0].outerHTML);
         var elements = document.getElementsByClassName("css-901oao css-16my406 r-1tl8opc r-ad9z0x r-bcqeeo r-qvutc0");
+        
         // elements.forEach(function(e, index) {
         //     console.log(index + ': ' + e.innerHTML);
         //   });
         
-        var user = "";
-        var id = "";
-        var body = "";
-        var date = "";
-        for(let i = 0; i < elements.length; i++) {
-            switch(i)
-            {
-            // case 10:
-            case 23: // 開発者ツールが有効か否かでインデックスが変わる
-                user = elements[i].innerHTML;
-                break;
-            // case 11:
-            case 24:
-                id = elements[i].innerHTML;
-                break;
-            // case 12:
-            case 25:
-                if(elements[i].innerHTML.search("<span class=") == -1) {
-                    body = elements[i].innerHTML;
+        if (request.messageType == "single-image") {
+            var user = "";
+            var id = "";
+            var body = "";
+            var date = "";
+            for(let i = 0; i < elements.length; i++) {
+                switch(i)
+                {
+                // case 10:
+                case 23: // 開発者ツールが有効か否かでインデックスが変わる
+                    user = elements[i].innerHTML;
+                    break;
+                // case 11:
+                case 24:
+                    id = elements[i].innerHTML;
+                    break;
+                // case 12:
+                case 25:
+                    if(elements[i].innerHTML.search("<span class=") == -1) {
+                        body = elements[i].innerHTML;
 
-                    // 改行をスペースに置き換え
-                    // 正規表現でないと全体の適用にならない
-                    body = body.replace(/\n/g, " ");
+                        // 改行をスペースに置き換え
+                        // 正規表現でないと全体の適用にならない
+                        body = body.replace(/\n/g, " ");
 
-                    // トリム
-                    body = body.trim();
-                }
-                break;
-            // case 13: // 本文がない場合はここが有効に
-            // case 14:
-            case 26:
-            case 27:
-                // 既に日付が入った場合は無視する
-                if(date == "") {
-                    var results = /((\d{4})年(\d{1,2})月(\d{1,2})日)/.exec(elements[i].innerHTML);
-                    if(results != null) {
-                        date = results[2] + zeroPadding(results[3], 2) + zeroPadding(results[4], 2);
+                        // トリム
+                        body = body.trim();
                     }
+                    break;
+                // case 13: // 本文がない場合はここが有効に
+                // case 14:
+                case 26:
+                case 27:
+                    // 既に日付が入った場合は無視する
+                    if(date == "") {
+                        var results = /((\d{4})年(\d{1,2})月(\d{1,2})日)/.exec(elements[i].innerHTML);
+                        if(results != null) {
+                            date = results[2] + zeroPadding(results[3], 2) + zeroPadding(results[4], 2);
+                        }
+                    }
+                    break;
                 }
-                break;
+                console.debug(i + ": " + elements[i].innerHTML);
             }
-            console.debug(i + ": " + elements[i].innerHTML);
-        }
-        var tweet = {
-            "user" : user,
-            "id" : id,
-            "body" : body,
-            "date": date
-        };
+            var tweet = {
+                "user" : user,
+                "id" : id,
+                "body" : body,
+                "date": date
+            };
 
-        if (request.messageType == "single-image")
             sendResponse({respondType: "single-image-request", tweet: tweet});
+        } else if (request.messageType == "multiple-image") {
+            var user = "";
+            var id = "";
+            var body = "";
+            var date = "";
+            for(let i = 0; i < elements.length; i++) {
+                switch(i)
+                {
+                case 23: // 開発者ツールが有効か否かでインデックスが変わる
+                    user = elements[i].innerHTML;
+                    break;
+                case 24:
+                    id = elements[i].innerHTML;
+                    break;
+                case 25:
+                    if(elements[i].innerHTML.search("<span class=") == -1) {
+                        body = elements[i].innerHTML;
+
+                        // 改行をスペースに置き換え
+                        // 正規表現でないと全体の適用にならない
+                        body = body.replace(/\n/g, " ");
+
+                        // トリム
+                        body = body.trim();
+                    }
+                    break;
+                case 26:
+                case 27:
+                    // 既に日付が入った場合は無視する
+                    if(date == "") {
+                        var results = /((\d{4})年(\d{1,2})月(\d{1,2})日)/.exec(elements[i].innerHTML);
+                        if(results != null) {
+                            date = results[2] + zeroPadding(results[3], 2) + zeroPadding(results[4], 2);
+                        }
+                    }
+                    break;
+                }
+                console.debug(i + ": " + elements[i].innerHTML);
+            }
+            var tweet = {
+                "user" : user,
+                "id" : id,
+                "body" : body,
+                "date": date
+            };
+
+            var elements = document.getElementsByClassName("css-1dbjc4n r-1p0dtai r-1mlwlqe r-1d2f490 r-11wrixw r-61z16t r-1udh08x r-u8s1d r-zchlnj r-ipm5af r-417010");
+            if(elements != null) {
+                tweet["images"] = [];
+                for(const e of elements) {
+                    var children = e.childNodes;
+                    console.debug("image src: " + children[1]["src"]);
+
+                    tweet["images"].push(children[1]["src"]);
+                }
+            }
+
+            sendResponse({respondType: "multiple-image-request", tweet: tweet});
+        }
     }
 );
